0f0a25b0aab455f4fca5a2057a011796418d15a7
akshobhya <akshobhyapal@gmail.com>
2020-03-30 18:38:37 +0530

    Improvements on UI

--- app/src/main/java/com/akshobhya/librehealth/Activity/SearchActivity.java
+++ app/src/main/java/com/akshobhya/librehealth/Activity/SearchActivity.java
@@ -1,8 +1,10 @@
 package com.akshobhya.librehealth.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,19 +12,28 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.akshobhya.librehealth.Adapter.SearchListAdapter;
 import com.akshobhya.librehealth.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.Calendar;
 
 public class SearchActivity extends AppCompatActivity {
 
-    private String name, date;
-    private EditText editText_search_name, editText_search_date;
+    private String name, date, did, result_id;
+    Context context;
+    private EditText editText_search_name, editText_search_date, editText_search_did;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        context = this;
+        editText_search_did = findViewById(R.id.editText_search_did);
         editText_search_name = findViewById(R.id.editText_search_name);
         editText_search_date = findViewById(R.id.editText_search_date);
     }
@@ -50,22 +61,36 @@ public class SearchActivity extends AppCompatActivity {
     public void button_search(View view) {
         date = editText_search_date.getText().toString();
         name = editText_search_name.getText().toString();
+        did = editText_search_did.getText().toString();
         if (date.isEmpty() || name.isEmpty()) {
             Toast.makeText(SearchActivity.this, "Please enter the details", Toast.LENGTH_LONG).show();
         } else {
-            Intent intent = new Intent(this, SearchListActivity.class);
-            intent.putExtra("SEARCH_NAME", name);
-            intent.putExtra("SEARCH_DATE", date);
-            startActivity(intent);
+            fetch_data();
         }
     }
 
-    public void button_showall(View view) {
-        name = "";
-        date = "";
-        Intent intent = new Intent(this, SearchListActivity.class);
-        intent.putExtra("SEARCH_NAME", name);
-        intent.putExtra("SEARCH_DATE", date);
-        startActivity(intent);
+    void fetch_data() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("child")
+                .whereEqualTo("name", name)
+                .whereEqualTo("dob", date)
+                .whereEqualTo("id", did)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                result_id = document.getString("id");
+                            }
+                            Intent intent = new Intent(context, SearchFormActivity.class);
+                            intent.putExtra("ID", result_id);
+                            context.startActivity(intent);
+                        } else {
+                            Toast.makeText(SearchActivity.this, "NOT FOUND", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 }
+
